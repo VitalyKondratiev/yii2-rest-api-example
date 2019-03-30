a58b1668613aba05c6040e099a202eb4e3c1ca92
Vitaly Kondratiev <kondratiev.vitaliy2012@yandex.ru>
2019-03-30 22:58:46 +0500

    Added console command 'task/index'
    Command works only with command-line args

---
+++ commands/TaskController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+use app\models\Calculation;
+
+/**
+ * This command calculates split point for array of numbers.
+ */
+class TaskController extends Controller
+{
+    /**
+    * @var int number for split rule.
+    */
+    public $number = 0;
+    /**
+    * @var array array for splitting.
+    */
+    public $data = [];
+    /**
+    * @var int user id for relation.
+    */
+    public $uid = null;
+
+    /**
+     * This command echoes split point and save equation to database.
+     * @return int Exit code
+     */
+    public function actionIndex()
+    {
+        $calculation = new Calculation();
+        $calculation->user_id = $this->uid;
+        $calculation->number = $this->number;
+        $calculation->data = $this->data;
+        $calculation->save();
+        echo $calculation->split_point_index . "\n";
+        return ExitCode::OK;
+    }
+
+    public function options($actionID)
+    {
+        return ['number', 'data', 'uid'];
+    }
+}
