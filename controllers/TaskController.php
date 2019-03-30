cd1e0adee20eefe029689a70dae3f8b5f43baa82
Vitaly Kondratiev <kondratiev.vitaliy2012@yandex.ru>
2019-03-30 22:08:47 +0500

    Split index calculation moved to Calculation model

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -37,45 +37,16 @@ class TaskController extends Controller
         $user_id = \Yii::$app->user->getId();
         $number = \Yii::$app->request->post('number') ?: null;
         $data = \Yii::$app->request->post('data') ?: [];
-        $split_point_index = self::getSplitPointIndex($number, $data);
-        $this->saveCalculation($user_id, $number, $data, $split_point_index);
+        $split_point_index = $this->makeCalculation($user_id, $number, $data, $split_point_index);
         return $split_point_index;
     }
 
-    private function saveCalculation($user_id, $number, $data, $split_point_index)
+    private function makeCalculation($user_id, $number, $data, $split_point_index)
     {
         $calculation = new Calculation();
         $calculation->user_id = $user_id;
         $calculation->number = $number;
         $calculation->data = $data;
-        $calculation->split_point_index = $split_point_index;
-        return $calculation->save();
-    }
-
-    /**
-     * Returns index before split point
-     */
-    private static function getSplitPointIndex($number, $data)
-    {
-        for ($split_point = 1; $split_point < count($data);  $split_point++)
-        {
-            $parts = [];
-            $parts['first'] = array_slice($data, 0, $split_point);
-            if (!in_array($number, $parts['first']))
-            {
-                continue;
-            }
-            $first_count = (array_count_values($parts['first'])[$number]);
-            $parts['second'] = array_slice($data, $split_point, count($data));
-            $second_count = (!in_array($number, $parts['second'])) ?
-                count($parts['second']) :
-                count($parts['second']) - (array_count_values($parts['second'])[$number]);
-            if ($first_count == $second_count)
-            {
-                return $split_point;
-                break;
-            }
-        }
-        return -1;
+        return $calculation->save() ? $calculation->split_point_index : null;
     }
 }
--- models/Calculation.php
+++ models/Calculation.php
@@ -15,6 +15,7 @@ use app\models\User;
  * @property int $user_id
  * @property int $number
  * @property array $data
+ * @property int $split_point_index
  * @property integer $created_at
  * @property integer $updated_at
  */
@@ -60,8 +61,38 @@ class Calculation extends ActiveRecord
         ];
     }
 
+    public function beforeSave($insert)
+    {
+        $this->split_point_index = self::calculateSplitPoint($this->number, $this->data);
+        return parent::beforeSave($insert);
+    }
+
     public function getUser()
     {
         return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    public static function calculateSplitPoint($number, $data)
+    {
+        for ($split_point = 1; $split_point < count($data);  $split_point++)
+        {
+            $parts = [];
+            $parts['first'] = array_slice($data, 0, $split_point);
+            if (!in_array($number, $parts['first']))
+            {
+                continue;
+            }
+            $first_count = (array_count_values($parts['first'])[$number]);
+            $parts['second'] = array_slice($data, $split_point, count($data));
+            $second_count = (!in_array($number, $parts['second'])) ?
+                count($parts['second']) :
+                count($parts['second']) - (array_count_values($parts['second'])[$number]);
+            if ($first_count == $second_count)
+            {
+                return $split_point;
+                break;
+            }
+        }
+        return -1;
     }
 }
